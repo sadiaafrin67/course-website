e8d283df71831bb4d71848832cc17e2c76657c42
sadiaafrin67 <sa540276@gmail.com>
2023-09-14 18:59:29 +0600

    add my first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      
+      <h1 className='text-6xl bg-slate-600'>Hellow world</h1>
+     
+    </>
+  )
+}
+
+export default App
